c300b871ae3df9bb2ab8d2b4be96ea63ee5d1ae0
Miguel Jiménez Gomis <alu0100970876@ull.edu.es>
2018-03-07 15:40:46 +0000

    First changes

--- src/Binks.java
+++ src/Binks.java
@@ -1,16 +1,36 @@
 import javax.swing.*;
+import java.util.*;
+import java.util.concurrent.ThreadLocalRandom;
 
 public class Binks{
-	public static void main(String[] args){
-        Timer timer = new Timer();
-        JFrame frame = new JFrame("Jar Jar");
+	JFrame frame;
+	Timer timer;
+	
+	final int MINPOS = 250;
+	final int MAXPOS = 750;
+	
+	Binks(){
+        this.timer = new Timer();
+		this.frame = new JFrame("Jar Jar");
         ImageIcon icon = new ImageIcon("resources/jarjar.jpg");
         JLabel label = new JLabel(icon);
-		timer.start();
-
-        frame.setSize(200,285);
+        frame.setSize(200, 285);
+        frame.setLocation(
+        		ThreadLocalRandom.current().nextInt(MINPOS, MAXPOS + 1),
+        		ThreadLocalRandom.current().nextInt(MINPOS, MAXPOS + 1));
         frame.setVisible(true);
         frame.add(label);
+		timer.start();
+	}
+	
+	
+	public static void main(String[] args){
+        ArrayList<Binks> jarjars = new ArrayList<Binks>();
+        for(int i = 0; i < 5; i++) {
+        	jarjars.add(new Binks());
+        }
+        
+        
 
 
 	}
